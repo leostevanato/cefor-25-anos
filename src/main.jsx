e6ef8e81cad7514def17c38937089537485581d7
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-20 15:19:10 -0300

    Criar seção depoimentos e componente botões nav

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import LinhaDoTempo from './LinhaDoTempo';
 import './js/main.js';
+import LinhaDoTempo from './LinhaDoTempo';
+import Depoimentos from './Depoimentos';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('depoimentos-cefor')).render(
+  <React.StrictMode>
+    <Depoimentos />
+  </React.StrictMode>
+);
 
 ReactDOM.createRoot(document.getElementById('timeline-cefor')).render(
   <React.StrictMode>
