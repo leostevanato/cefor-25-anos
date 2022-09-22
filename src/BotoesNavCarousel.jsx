714aa9d23eed98227c7cd6fb7e53ad8dd2bd31d1
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-22 15:31:55 -0300

    Atualizar componentes dos botões de navegação

--- src/BotoesNavCarousel.jsx
+++ src/BotoesNavCarousel.jsx
@@ -5,9 +5,9 @@ export default ({ next, previous, goToSlide, ...rest }) => {
 
 	return (
 		<div className="carousel-button-group position-absolute d-flex mt-4 gap-4 start-50">
-			<ButtonSeta className="btn seta esquerda border-0" disabled={ currentSlide === 0 } funcaoClick={ () => previous() } />
+			<ButtonSeta className="btn seta esquerda border-0" disabled={ currentSlide === 0 } onClick={ () => previous() } />
 
-			<ButtonSeta direcao="direita" className="btn seta direita border-0" disabled={ currentSlide === (totalItems - slidesToShow) } funcaoClick={ () => next() } />
+			<ButtonSeta direcao="direita" className="btn seta direita border-0" disabled={ currentSlide === (totalItems - slidesToShow) } onClick={ () => next() } />
 		</div>
 	);
 };
--- src/ButtonSeta.jsx
+++ src/ButtonSeta.jsx
@@ -4,7 +4,7 @@ ButtonSeta.defaultProps = {
 	corSeta: "#E8E8E7",
 	disabled: false,
 	direcao: 'esquerda', // 'esquerda' ou 'direita'
-	funcaoClick: () => {},
+	onClick: () => {},
 	altura: "40",
 	largura: "40"
 };
@@ -13,7 +13,7 @@ function ButtonSeta(props) {
 	let direcaoStyle = (props.direcao == 'direita') ? { transform: 'scaleX(-1)' } : {};
 	
 	return (
-		<button type="button" style={ direcaoStyle } className={ props.className } disabled={ props.disabled } onClick={ props.funcaoClick }>
+		<button type="button" style={ direcaoStyle } className={ props.className } disabled={ props.disabled } onClick={ props.onClick }>
 			<svg width={ props.largura } height={ props.altura } viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<circle cx="20" cy="20" r="20" fill={ props.corFundo } />
 				<path d="M23.75 10.8333L15 19.5833L23.75 28.3333" stroke={ props.corSeta } strokeWidth="6" />
