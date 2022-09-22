57e557561a74b45fc36295fc25500e9c642b539e
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-22 16:35:00 -0300

    Criar seção Cefor em números

---
+++ src/CeforEmNumeros.jsx
@@ -0,0 +1,81 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import React from 'react';
+import ButtonSeta from "./ButtonSeta";
+
+const imagensCeforNumeros = [
+	{
+		imagem: 'cefor_em_numeros_1.png',
+		alt: '1'
+	},
+	{
+		imagem: 'cefor_em_numeros_2.png',
+		alt: '2'
+	},
+	{
+		imagem: 'cefor_em_numeros_3.png',
+		alt: '3'
+	},
+	{
+		imagem: 'cefor_em_numeros_4.png',
+		alt: '4'
+	},
+	{
+		imagem: 'cefor_em_numeros_5.png',
+		alt: '5'
+	},
+	{
+		imagem: 'cefor_em_numeros_6.png',
+		alt: '6'
+	}
+];
+
+const listaImagens = imagensCeforNumeros.map((item, index) => {
+	return <div key={ index } className="card-imagem mx-auto">
+		<img className="img-fluid mx-auto d-block" src={ '/src/cefor_em_numeros/' + item.imagem } alt={ item.alt } />
+	</div>
+});
+
+const responsive = {
+	desktop: {
+		breakpoint: { max: 4000, min: 1025 },
+		items: 1
+	},
+	tablet_e_mobile: {
+		breakpoint: { max: 1024, min: 0 },
+		items: 1
+	}
+};
+
+const CustomSetaEsquerda = ({ onClick, ...rest }) => {
+	const { carouselState: { currentSlide } } = rest;
+
+	return <ButtonSeta
+		className="btn carousel-seta esquerda border-0"
+		disabled={ currentSlide === 0 }
+		onClick={ () => onClick() }
+	/>
+};
+
+const CustomSetaDireita = ({ onClick, ...rest }) => {
+	const { carouselState: { currentSlide, totalItems, slidesToShow } } = rest;
+
+	return <ButtonSeta
+		direcao="direita"
+		className="btn carousel-seta direita border-0"
+		disabled={ currentSlide === (totalItems - slidesToShow) }
+		onClick={ () => onClick() }
+	/>
+};
+
+export default () => (
+	<Carousel
+		responsive={ responsive }
+		renderArrowsWhenDisabled={ true }
+		removeArrowOnDeviceType="tablet_e_mobile"
+		customLeftArrow={ <CustomSetaEsquerda /> }
+		customRightArrow={ <CustomSetaDireita /> }
+	>
+		{ listaImagens }
+	</Carousel>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './js/main.js';
 import LinhaDoTempo from './LinhaDoTempo';
 import Depoimentos from './Depoimentos';
+import CeforEmNumeros from './CeforEmNumeros';
 
 ReactDOM.createRoot(document.getElementById('depoimentos-cefor')).render(
   <React.StrictMode>
@@ -14,4 +15,8 @@ ReactDOM.createRoot(document.getElementById('timeline-cefor')).render(
   <React.StrictMode>
     <LinhaDoTempo />
   </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById('cefor-em-numeros-carousel')).render(
+  <CeforEmNumeros />
 );
