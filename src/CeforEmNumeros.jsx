d5452ce71c77871e08ddeca49f01df6100ddc244
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-26 11:23:59 -0300

    Tornar seção Cefor em números responsiva

--- src/CeforEmNumeros.jsx
+++ src/CeforEmNumeros.jsx
@@ -68,13 +68,25 @@ const CustomSetaDireita = ({ onClick, ...rest }) => {
 	/>
 };
 
+const BotoesNavCarousel = ({ next, previous, goToSlide, ...rest }) => {
+	const { carouselState: { currentSlide, totalItems, slidesToShow } } = rest;
+
+	return (
+		<div className="botoes-nav-carousel carousel-button-group position-absolute d-flex container-lg">
+			<ButtonSeta className="btn seta esquerda border-0" corFundo="#51BABB" corSeta="#0A312C" disabled={ currentSlide === 0 } onClick={ () => previous() } />
+
+			<ButtonSeta direcao="direita" className="btn seta direita border-0" corFundo="#51BABB" corSeta="#0A312C" disabled={ currentSlide === (totalItems - slidesToShow) } onClick={ () => next() } />
+		</div>
+	);
+};
+
 export default () => (
 	<Carousel
 		responsive={ responsive }
 		renderArrowsWhenDisabled={ true }
-		removeArrowOnDeviceType="tablet_e_mobile"
-		customLeftArrow={ <CustomSetaEsquerda /> }
-		customRightArrow={ <CustomSetaDireita /> }
+		arrows={ false }
+		renderButtonGroupOutside={true}
+		customButtonGroup={ <BotoesNavCarousel /> }
 	>
 		{ listaImagens }
 	</Carousel>
