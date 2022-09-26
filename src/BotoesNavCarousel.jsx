342e4affe69a3b1fe2501688b99ffffeb4ee7300
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-26 11:22:51 -0300

    Corrigir alinhamento dos botões

--- src/BotoesNavCarousel.jsx
+++ src/BotoesNavCarousel.jsx
@@ -4,7 +4,7 @@ export default ({ next, previous, goToSlide, ...rest }) => {
 	const { carouselState: { currentSlide, totalItems, slidesToShow } } = rest;
 
 	return (
-		<div className="carousel-button-group position-absolute d-flex mt-4 gap-4 start-50">
+		<div className="carousel-button-group position-absolute d-flex mt-4 gap-4 start-50 translate-middle-x">
 			<ButtonSeta className="btn seta esquerda border-0" disabled={ currentSlide === 0 } onClick={ () => previous() } />
 
 			<ButtonSeta direcao="direita" className="btn seta direita border-0" disabled={ currentSlide === (totalItems - slidesToShow) } onClick={ () => next() } />
