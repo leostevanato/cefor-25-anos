bfa3242cfc6c4378892c15f73fe01e1703dbdf65
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-30 08:31:07 -0300

    Backup de arquivos usados para gerar a linha do tempo

---
+++ src/js/linhaDoTempo copy.js
@@ -0,0 +1,225 @@
+const linhaTempoAntes = [
+	{
+	  "title": 1971,
+	  "cardTitle": "Criada a <b>Coordenação de Seleção e Treinamento</b> para atender exclusivamente os servidores da Câmara dos Deputados",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 1997,
+	  "cardTitle": "<b>Institucionalização do</b> Centro de Formação, Treinamento e Aperfeiçoamento – <b>Cefor</b>",
+	  "cardDetailedText": "O Cefor tem com a missão qualificar os servidores da Câmara dos Deputados e promover a Educação Legislativa, com vistas ao fortalecimento da democracia com foco no poder legislativo."
+	},
+	{
+	  "title": 2000,
+	  "cardTitle": "Último <b>concurso público</b> realizado pelo Cefor",
+	  "cardDetailedText": "A partir de então, o Cefor passa a atuar na contratação e fiscalização de concursos feitos com realização externa."
+	},
+	{
+	  "title": 2000,
+	  "cardTitle": "É ofertada a <b>primeira especialização para gestores</b> da Câmara dos Deputados, em parceria com a Universidade de Brasília",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2000,
+	  "cardTitle": "Oferta das primeiras capacitações em “<b>Processo Orçamentário</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2001,
+	  "cardTitle": "Criado o <b>Ensino a Distância</b> da Câmara dos Deputados",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2001,
+	  "cardTitle": "Oferta das primeiras capacitações em <b>Processo Legislativo</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2002,
+	  "cardTitle": "Lançado o <b>Programa de Desenvolvimento dos Secretários Parlamentares</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2002,
+	  "cardTitle": "Lançado o “<b>Programa de Desenvolvimento da Coordenação de Segurança Legislativa</b>” (atual DEPOL)",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2003,
+	  "cardTitle": "Primeira edição do “<b>Estágio-Visita</b>”",
+	  "cardDetailedText": "Programa educativo que busca aproximar os universitários do Parlamento, a partir de uma experiência interativa e dinâmica em que os jovens participam de aulas, debates, rodas de conversa, simulações e observação do trabalho parlamentar."
+	},
+	{
+	  "title": 2004,
+	  "cardTitle": "Primeira edição do “<b>Parlamento Jovem Brasileiro</b>”",
+	  "cardDetailedText": "Programa educativo de simulação parlamentar em que jovens de todo o Brasil se reúnem na Câmara dos Deputados para analisar, debater e deliberar sobre projetos de lei de sua autoria."
+	},
+	{
+	  "title": 2005,
+	  "cardTitle": "Institucionalização do <b>Programa de Pós-Graduação</b> da Câmara dos Deputados",
+	  "cardDetailedText": "A partir do credenciamento do Cefor junto ao MEC para oferta de cursos lato sensu, o programa oferece suas duas primeiras especializações: “Processo Legislativo” e “Instituições e Processos Políticos do Legislativo"
+	},
+	{
+	  "title": 2006,
+	  "cardTitle": "Oferta de cursos a distância para a <b>Associação de Secretários-Gerais dos Países de Língua Portuguesa</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2006,
+	  "cardTitle": "Criação da “<b>Coordenação de Pós-Graduação</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2006,
+	  "cardTitle": "Lançado o “<b>Programa de Desenvolvimento Gerencial</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2006,
+	  "cardTitle": "Lançamento dos primeiros <b>Grupos de Pesquisa e Extensão</b> do Programa <b>de Pós-Graduação</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2008,
+	  "cardTitle": "Criação da <b>Coordenação de Educação para Democracia</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2008,
+	  "cardTitle": "Lançamento da <b>Revista Eletrônica E-Legis</b> do Programa de Pós-Graduação",
+	  "cardDetailedText": "A E-Legis é a única revista brasileira com foco no Poder Legislativo"
+	},
+	{
+	  "title": 2009,
+	  "cardTitle": "EaD da Câmara ganha o <b>prêmio</b> E-Learning Brasil na categoria '<b>Contribuição Marcante</b>'",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2009,
+	  "cardTitle": "Primeira edição do “<b>Escola na Câmara</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2010,
+	  "cardTitle": "Realizada a <b>I Jornada de Pesquisa e Extensão</b> do Programa <b>de Pós-Graduação</b> da Câmara dos Deputados",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2010,
+	  "cardTitle": "EaD da Câmara ganha o <b>prêmio</b> E-Learning Brasil na categoria '<b>Referência Nacional</b>'",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2011,
+	  "cardTitle": "Primeira edição do “<b>Missão Pedagógica no Parlamento</b>”",
+	  "cardDetailedText": "Programa de formação de educadores da rede pública de ensino focado no desenvolvimento de práticas pedagógicas de educação para democracia."
+	},
+	{
+	  "title": 2011,
+	  "cardTitle": "Primeira edição do “<b>Estágio-Cidadão</b>”",
+	  "cardDetailedText": "Programa educativo volta aos estagiários da Câmara dos Deputados, em que universitários de diferentes áreas aprendem sobre o processo legislativo e as funções do Parlamento."
+	},
+	{
+	  "title": 2012,
+	  "cardTitle": "Primeira edição da “<b>Oficina de Atuação no Parlamento</b>”",
+	  "cardDetailedText": "Programa educativo que apresenta formas para que a sociedade civil organizada possa acompanhar o processo legislativo na prática e participar do processo de elaboração de leis."
+	},
+	{
+	  "title": 2012,
+	  "cardTitle": "Lançado o <b>Canal Cefor-VÍDEO</b> no YouTube",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2013,
+	  "cardTitle": "Início do <b>Mestrado Profissional em Poder Legislativo</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2013,
+	  "cardTitle": "Primeira edição do curso “<b>Educação para Democracia e o Parlamento</b>”",
+	  "cardDetailedText": "Curso introdutório voltado para educadores da rede pública de ensino."
+	},
+	{
+	  "title": 2014,
+	  "cardTitle": "Lançado o “<b>Programa de Educação Continuada em Gestão</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2015,
+	  "cardTitle": "Primeira edição do “<b>Estágio Participação</b>”",
+	  "cardDetailedText": "Programa educativo que incentiva a participação política e cidadã por meio acompanhamento das atividades parlamentares e busca ativa de informações sobre o Legislativo."
+	},
+	{
+	  "title": 2017,
+	  "cardTitle": "Lançado o “<b>Programa de Educação Continuada em Aquisições</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2017,
+	  "cardTitle": "<b>Pós-graduação</b> é recredenciada a ofertar de cursos <em>lato sensu </em>nas modalidades <b>presencial e a distância</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2018,
+	  "cardTitle": "Lançamento do “<b>Clique Regimento</b>”",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2019,
+	  "cardTitle": "Lançamento da nova plataforma de Ensino a Distância da Câmara dos Deputados: <b>ELEVE</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2020,
+	  "cardTitle": "Lançamento da nova versão do <b>Portal EVC</b>",
+	  "cardDetailedText": "Portal educativo com conteúdo multimidia de Educação para Democracia"
+	},
+	{
+	  "title": 2020,
+	  "cardTitle": "<b>Mudança de paradigma</b>. Cursos e atividades presenciais do Cefor passam a ser realizados a distância",
+	  "cardDetailedText": "A adaptação for necessária por conta do isolamento social durante a pandemia de COVID-19"
+	},
+	{
+	  "title": 2021,
+	  "cardTitle": "O <b>Plenarinho</b> - portal infanto-juvenil da Câmara dos Deputados - passa a integrar os projetos de educação para democracia do Cefor",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2021,
+	  "cardTitle": "São criadas as <b>redes sociais da Escola da Câmara</b> para divulgar as atividades e os conteúdos educativos produzidos pelo Cefor",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2022,
+	  "cardTitle": "Cursos do Cefor passam a integrar a plataforma <b>EVG/Enap</b>",
+	  "cardDetailedText": ""
+	},
+	{
+	  "title": 2022,
+	  "cardTitle": "Canal da Escola da Câmara no Youtube supera os <b>20 mil inscritos</b>",
+	  "cardDetailedText": ""
+	}
+];
+
+var linhaTempoJSON = [];
+
+linhaTempoAntes.forEach(item => {
+	let negritos = item.cardTitle.match(/<b>(.*?)<\/b>/g);
+
+	let titulo = '<b>' + item.title + '</b><br>';
+	let tituloCard = '<b>' + item.title + '<br>' + item.cardTitle.replace(/<b>|<\/b>/gi, '') + '</b>';
+
+	negritos.forEach(elemento => {
+		titulo += elemento.replace(/<b>|<\/b>/gi, '') + ' ';
+	});
+
+	titulo = titulo.trim();
+
+	linhaTempoJSON.push({
+		"title": titulo,
+		"cardTitle": tituloCard,
+		"cardDetailedText": item.cardDetailedText
+	});
+});
+
+export default linhaTempoJSON;
